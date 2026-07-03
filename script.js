d7545d677d3fc4d7b1277f1bda36cbc66f1da057
zainulrehman067 <zainulrehman067@gmail.com>
2026-07-03 06:13:46 +0500

    Add JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+// Study With Saim JavaScript
+
+console.log("Welcome to Study With Saim!");
+
+const button = document.querySelector("button");
+
+if (button) {
+    button.addEventListener("click", function () {
+        alert("Welcome to Study With Saim! جلد ہی تمام نوٹس دستیاب ہوں گے۔");
+    });
+}
